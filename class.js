9b0549b5c933a9bfd436299031fa04f6ea9d5852
ravibs298 <ravibs2908@gmail.com>
2025-04-22 13:11:07 +0530

    Add files via upload
    Methods, Properties and Getters & Setters

--- class.js
+++ class.js
@@ -1,16 +1,3 @@
-//CLASSES IN JS 
-//Classes and Objects 
-//Classes are a template for creating objects 
-//Objects are instances of a class 
-//Classes are used to create objects that have similar properties and methods
-//Elements of a class => constructor
-// let dog = {
-//     name:"Javascript",
-//     weight:10,
-//     color:"white",
-//     breed:"Golden Retriever",
-// };
-
 // class ClassName {
 //     constructor(prop1,prop2){
 //         this.prop1 = prop1;
@@ -73,4 +60,71 @@
 // console.log(me.lastname);
 // console.log(me);
 // me.greet();
-// console.log(me.compliment("Javascript","dog"));
+
+//Properties 
+//proprties are sometimes called fields,hold data for the class
+//it is not possible to access the properties directly , but can be accessed by methods
+
+// class person{
+//     #firstname;//to make a variable private 
+//     #lastname;
+//     constructor(firstname,lastname){
+//         this.#firstname = firstname;
+//         this.#lastname = lastname;
+//     }
+// }
+// let p = new person("Ravi","Singh");
+// console.log(p.firstname);//undefined 
+// console.log(p.#firstname);//error
+
+// class person{
+//     #firstname;//to make a variable private 
+//     #lastname;
+//     constructor(firstname,lastname){
+//         if(firstname.startsWith("N")){
+//             this.#firstname = firstname;
+//     }
+//     else{
+//         this.#firstname = "M."+firstname;
+//     }
+//     this.#lastname = lastname;
+// }
+//   getfullname(){
+//     return this.#firstname+" "+this.#lastname;
+//   }
+// }
+// let p = new person("Bhanu","Singh");
+// console.log(p.getfullname());
+
+//Getters and Setters 
+//private members are always accessed through methods 
+//public members can be asscessed directly by objects 
+//class can have both public and private members
+
+class person{
+    #firstname;//to make a variable private 
+    #lastname;
+    constructor(firstname,lastname){
+        this.#firstname = firstname;
+        this.#lastname = lastname;
+    }
+    get firstname(){
+        return this.#firstname;
+    }
+    set firstname(firstName){
+        this.#firstname = firstName;
+    }
+    get lastname(){
+        return this.#lastname;
+    }
+    set lastname(lastName){
+        this.#lastname = lastName;
+    }
+}
+let p = new person("Ravi","Singh");
+console.log(p.firstname);
+console.log(p.lastname);
+p.firstname = "Bhanu";
+console.log(p.firstname);
+p.lastname = "Singh";
+console.log(p.lastname);
