8a7220883e86fe81de5cb2d77bbe4b23626afb39
ravibs298 <ravibs2908@gmail.com>
2025-04-23 16:05:40 +0530

    Add files via upload
    Maps

---
+++ maps.js
@@ -0,0 +1,60 @@
+// Maps are used to store key value pairs, where key can be of ant data type
+//  const fruits = new Map([
+//         ["apples",500],
+//         ["bananas",300],
+//         ["oranges",400]
+//     ]);
+//      fruits.set("apples",100);
+//      console.log(fruits);
+     const fruits = new Map();
+fruits.set("Apple",500);
+fruits.set("Banana",300);
+fruits.set("Orange",400);
+// console.log(fruits);
+// fruits.set("Apple",1000);
+// console.log(fruits);
+
+// //The get() method 
+// console.log(fruits.get("Apple"));
+// console.log(fruits.size);
+// console.log(fruits.delete("Apple"));
+// fruits.clear();
+console.log(fruits);
+console.log(fruits.has("Apple"));
+
+//Difference b/w Object & map 
+//Object is a collection of key value pairs & Map is a collection of key value pairs 
+//Object is used to store data in key value pairs & Map is used to store data in key value pairs
+//object do not have  the size property and Maps has size property 
+//Objects have default kkeys and Maps do not have default keys 
+
+// let text = "";
+// fruits.forEach((value,key)=>{
+//     text += key+" "+value+" ";
+// });
+// console.log(text);
+// console.log(typeof(text));
+let text = "";
+// for(const x of fruits.entries()){
+//     text += x;
+// }
+// console.log(text);
+// console.log(typeof(text));
+// for(const x of fruits.keys()){
+//     text += x;
+// }
+// console.log(text);
+// console.log(typeof(text));
+// for(const x of fruits.values()){
+//     text += x;
+// }
+// console.log(text);
+// console.log(typeof(text));
+// Sum of all values 
+let sum = 0;
+for(const x of fruits.values()){
+    sum += x;
+}
+console.log(sum);
+console.log(typeof(sum));
+
